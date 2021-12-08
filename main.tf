80fd504855010c87d49e9223bb56e179dd81eb2a
RJ <ravichanderkt@gmail.com>
2021-12-08 14:35:38 +0000

    Application & DB instance logic added

--- main.tf
+++ main.tf
@@ -1,45 +1,95 @@
 provider "google" {
-  region = "${var.region}"
+  region = var.region
 }
 data "google_compute_zones" "available" {
-  region = "${var.region}"
+  region = var.region
   status = "UP"
 }
 
+locals {
+  is_prod = length(regexall("p", "${var.instance_name}")) > 0
+  is_qa = length(regexall("q", "${var.instance_name}")) > 0
+}
+
 resource "google_compute_address" "instances" {
-  count = "${var.amount}"
-  name  = "${var.name_prefix}-${count.index}"
+  count = var.amount
+  name  = "${var.instance_name}-${count.index}"
 }
 
-resource "google_compute_disk" "instances" {
-  count = "${var.amount}"
+# resource "google_compute_disk" "instances" {
+#   name = var.instance_name
+#   type = var.disk_type
+#   size = var.disk_size
+#   zone = var.zone
 
-  name = "${var.name_prefix}-${count.index+1}"
-  type = "${var.disk_type}"
-  size = "${var.disk_size}"
-  zone = "${data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)]}"
+#   image = var.disk_image
 
-  image = "${var.disk_image}"
-
+# }
+resource "google_compute_disk" "additional_disk" {
+  count = "${local.is_prod}" ? 1 : 0
+  name  = "test-disk"
+  type  = "pd-ssd"
+  zone  = var.zone
+  size  = 10
 }
 
-resource "google_compute_instance" "instances" {
-  count = "${var.amount}"
+resource "google_compute_instance" "prod_instances" {
+  count = "${local.is_prod}" ? 1 : 0
 
-  name         = "${var.name_prefix}-${count.index+1}"
-  zone         = "${var.zone}"
-  machine_type = "${var.machine_type}"
-
+  name                = var.instance_name
+  zone                = var.zone
+  machine_type        = var.machine_type
   boot_disk {
-    source      = "${google_compute_disk.instances.*.name[count.index]}"
-    auto_delete = false
+    initialize_params {
+      image       = "{var.disk_image}"
+      type        = "pd-ssd"
+    }
+  }
+  deletion_protection = true
+  tags                = "${var.tags}"
+  attached_disk {
+    source = google_compute_disk.additional_disk.*.name[count.index]
   }
 
+
+
   network_interface {
-    network = "default"
+    network = var.network
 
     access_config {
-      nat_ip = "${google_compute_address.instances.*.address[count.index]}"
+      nat_ip = google_compute_address.instances.*.address[count.index]
+    }
+  }
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+  scheduling {
+    on_host_maintenance = "MIGRATE"
+    automatic_restart   = "true"
+  }
+}
+
+# Creating the QA GCE
+resource "google_compute_instance" "qa_instances" {
+  count = "${local.is_qa}" ? 1 : 0
+
+  name                = var.instance_name
+  zone                = var.zone
+  machine_type        = var.machine_type
+  boot_disk {
+    initialize_params {
+      image       = "{var.disk_image}"
+      type        = "pd-ssd"
+    }
+  }
+  deletion_protection = true
+  tags                = "${var.qa_tags}"
+  
+  network_interface {
+    network = var.network
+
+    access_config {
+      nat_ip = google_compute_address.instances.*.address[count.index]
     }
   }
   service_account {
--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "region" {
-    default = "us-west4"
+  default = "us-west4"
 }
 variable "zone" {
-    default = "us-west4-a"
+  default = "us-west4-a"
 }
 
 variable "amount" {
-    default = "1"
+  default = "1"
 }
-variable "name_prefix" {
-    default = "rj"
+variable "instance_name" {
+  default = "p-a-cos"
 }
 variable "machine_type" {
-    default = "n2-standard-2"
+  default = "n2-standard-2"
 }
 
 variable "disk_type" {
@@ -20,8 +20,17 @@ variable "disk_type" {
 }
 
 variable "disk_size" {
-    default = "50"
+  default = "50"
 }
 variable "disk_image" {
-    default = "windows-server-20h2-dc-core-v20211115"
+  default = "windows-server-20h2-dc-core-v20211115"
+}
+variable "network" {
+  default = "default"
+}
+variable "tags" {
+   default = ["rj", "rj"]
+}
+variable "qa_tags" {
+   default = ["qa_rj", "qa_rj"]
 }
